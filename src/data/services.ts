7013acb625a78ae3800cd199df07c3accf042ccf
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2025-02-02 16:23:32 -0300

    First Commit

---
+++ src/data/services.ts
@@ -0,0 +1,40 @@
+import type { Props as ServicesProp } from "../components/service/props";
+
+export const SERVICES: ServicesProp[] = [
+  {
+    title: "Desenvolvimento de Software",
+    description:
+      "Criação e manutenção de sistemas personalizados, garantindo soluções eficientes e escaláveis para atender às necessidades do seu negócio.",
+    logo: "/logos/software-development.png",
+  },
+  {
+    title: "Monitoramento e Otimização de Performance",
+    description:
+      "Análise e aprimoramento do desempenho das aplicações, reduzindo tempo de resposta e melhorando a experiência do usuário.",
+    logo: "/logos/performance-monitoring.png",
+  },
+  {
+    title: "Gerenciamento e Suporte para Times de Desenvolvimento",
+    description:
+      "Apoio técnico e estratégico para equipes de desenvolvimento, garantindo eficiência, colaboração e qualidade nas entregas.",
+    logo: "/logos/team-management.png",
+  },
+  {
+    title: "Implementação de Metodologias Ágeis",
+    description:
+      "Adoção de práticas ágeis como Scrum e Kanban para melhorar a organização, previsibilidade e entrega contínua de valor.",
+    logo: "/logos/agile-methodologies.png",
+  },
+  {
+    title: "Definição e Acompanhamento de Métricas",
+    description:
+      "Monitoramento de qualidade do código e produtividade da equipe, utilizando métricas estratégicas para tomadas de decisão baseadas em dados.",
+    logo: "/logos/code-metrics.png",
+  },
+  {
+    title: "Melhoria na Arquitetura e Escalabilidade",
+    description:
+      "Revisão e otimização da arquitetura do software para garantir um sistema robusto, seguro e preparado para o crescimento do negócio.",
+    logo: "/logos/architecture-scalability.png",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,15 @@
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconWhatsapp from "../components/icons/whatsapp.astro";
+
+export const SOCIALS = [
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/vrusin/",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Whatsapp",
+    url: "https://api.whatsapp.com/send?phone=5577998292477&text=Ol%C3%A1%20Marco,%20tenho%20interesse%20em%20saber%20mais%20sobre%20o%20seu%20servi%C3%A7o%20de%20desenvolvimento%20de%20softwares.%20Podemos%20conversar?%F0%9F%91%8B",
+    icon: IconWhatsapp,
+  },
+];
